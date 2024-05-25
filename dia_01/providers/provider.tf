8191c8d4df98d800f04ec7fb3401297ea2158ccc
Michel Ferreira <mi.apferreira@gmail.com>
2024-05-25 20:00:28 -0300

    first-class

---
+++ dia_01/providers/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-estudoss"
+    key    = "aula_providers"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "sampa"
+  region = "sa-east-1"
+}
